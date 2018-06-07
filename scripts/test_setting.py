5f2785a70a72effb7235be77f1aff0d2bac0e2fc
John Doe <johndoe@example.com>
2018-06-07 16:07:47 +0800

    4 commit

---
+++ scripts/test_setting.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# cy
+
+from appium import webdriver
+
+desired_caps = {}
+
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '5.1'
+desired_caps['deviceName'] = '192.168.56.101:5555'
+desired_caps['appPackage'] = 'com.android.settings'
+desired_caps['appActivity'] = '.Settings'
+desired_caps['unicodeKeyboard'] = True
+desired_caps['resetKeyboard'] = True
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
